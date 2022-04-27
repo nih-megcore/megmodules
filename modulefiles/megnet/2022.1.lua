e49ed0d61f718cfd64f88dcb8a673113e436738a
Jeff Stout <stoutjd@nih.gov>
2022-04-27 11:34:15 -0400

    Added megnet module

---
+++ modulefiles/megnet/2022.1.lua
@@ -0,0 +1,17 @@
+local version = myModuleVersion()
+local base = "/data/ML_MEG/python_modules/megnet" .. version
+
+family("MEGnet")
+prepend_path("PATH", pathJoin(base,"bin"))
+
+local omp_nt = os.getenv("OMP_NUM_THREADS") or "1"
+pushenv("OMP_NUM_THREADS", omp_nt)
+
+if (mode() == "load") then
+    LmodMessage("[+] Loading MEGnet ",version," ...")
+    LmodMessage("[+] Available: ")
+    LmodMessage("[+]   ICA.py")
+end
+if (mode() == "unload") then
+    LmodMessage("[-] Unloading MEGnet ",version," ...")
+end
