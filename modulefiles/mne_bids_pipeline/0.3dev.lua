618aa1f8f60afcad20c46cbe451f9897e84378f9
Jeff Stout <stoutjd@nih.gov>
2022-08-10 20:54:23 -0400

    Added mne_bids_pipeline w/ update

---
+++ modulefiles/mne_bids_pipeline/0.3dev.lua
@@ -0,0 +1,31 @@
+always_load('freesurfer')
+always_load('Xvfb')
+always_load('mesa')
+
+local version = myModuleVersion()
+local base = "/vf/users/MEGmodules/modules/mne_bids_pipeline" .. version
+local scripts = base .. "_extras/scripts"
+
+family("mne_bids_pipeline")
+prepend_path("PATH", pathJoin(base,"bin"))
+prepend_path("PATH", scripts)
+
+local omp_nt = os.getenv("OMP_NUM_THREADS") or "1"
+pushenv("OMP_NUM_THREADS", omp_nt)
+
+if (mode() == "load") then
+    LmodMessage("[+] Loading mne_bids_pipeline ",version," ...")
+    LmodMessage('[+] ')
+    LmodMessage('[+] !! Make sure you have an lscratch if performing report gen!!')
+    LmodMessage('[+]    (prior to loading this module you should have initiated like below)')
+    LmodMessage('[+]    e.g.: ')
+    LmodMessage('[+]      sinteractive --mem=6G --cpus-per-task=4 --gres=lscratch:50 ')
+    LmodMessage('[+] ')
+    LmodMessage("[+] Available Scripts")
+    LmodMessage("[+]   mne-bids-pipeline-run.py")
+end
+if (mode() == "unload") then
+    LmodMessage("[-] Unloading mne_bids_pipeline ",version," ...")
+end
+setenv("MESA_GL_VERSION_OVERRIDE",3.3)
+setenv("MNE_3D_OPTION_ANTIALIAS",'false')
