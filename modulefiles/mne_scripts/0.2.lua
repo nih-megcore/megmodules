c68335850f6e8e620ca919d5cbb33380ab3d4dc9
Jeff Stout <stoutjd@nih.gov>
2023-10-19 14:37:24 -0400

    Updates for bids anonymization

---
+++ modulefiles/mne_scripts/0.2.lua
@@ -0,0 +1,33 @@
+always_load('mne/1.3')
+always_load('ctf')
+
+local version = myModuleVersion()
+local base = "/vf/users/MEGmodules/modules/mne_scripts" .. version
+
+
+family("mne_scripts")
+prepend_path("PATH", base) 
+
+
+local omp_nt = os.getenv("SLURM_CPUS_PER_TASK") or "1"
+pushenv("OMP_NUM_THREADS", omp_nt)
+
+if (mode() == "load") then
+    LmodMessage("[+] Loading mne_scripts ",version," ...")
+    LmodMessage("Available:")
+    LmodMessage(" spatiotemporal_clustering_stats.py")
+    LmodMessage(" make_meg_bids.py")
+    LmodMessage(" bstags.py")
+    LmodMessage(" calc_mnetrans.py")
+    LmodMessage(" make_bids_fs_swarm.sh")
+    LmodMessage(" bids_mne_preproc_mri.py")
+    LmodMessage(" ")
+    LmodMessage(" ")
+
+    LmodMessage("WARNING!! - This will not anonymize data!! Use version 0.3+")
+    LmodMessage("WARNING!! - if you run into issues/errors with mixing BIDS versions")
+    LmodMessage("do:  module load mne_scripts/0.1_dev")
+end
+if (mode() == "unload") then
+    LmodMessage("[-] Unloading mne_scripts ",version," ...")
+end
